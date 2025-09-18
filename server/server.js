ff66ce9f5c1322cdf56dc4cd249f883b84c7ceb6
rohini123-k <rohinikothalkar123@gmail.com>
2025-09-18 09:55:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,8 @@ const corsOptions = {
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:3001',
-    'http://127.0.0.1:3001'
+    'http://127.0.0.1:3001',
+    'https://finance-tracker-app-1-riwz.onrender.com' // ✅ frontend Render URL
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
