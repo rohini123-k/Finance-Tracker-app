6361e7f688bccb4ca0608b76221ea80eff84c3d4
rohini123-k <rohinikothalkar123@gmail.com>
2025-09-18 10:17:42 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://finance-tracker-app-ihdp.onrender.com/api';
-const API_BASE_URL = 'https://finance-tracker-app-ihdp.onrender.com/api';
+// const API_BASE_URL = 'https://finance-tracker-app-ihdp.onrender.com/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
